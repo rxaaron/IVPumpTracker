3787505eb5f18c1d8f96917d829bbdb9db222095
ataylor <ataylor@GMAP_Encore_2.grcs.local>
2014-02-28 10:40:31 -0500

    Added pumps

--- IV Pump Tracker/index.php
+++ IV Pump Tracker/index.php
@@ -13,12 +13,10 @@
     <body>
 <div class="content">
   <center>
-    <h2>IV Pump Locations</h2>
+    <h2>IV Pump Locations <button id="refreshbtn"><i class="fa fa-refresh"></i> Refresh</button></h2>
     <table class="pure-table pure-table-bordered" id="locationtable">
     <?php include_once('scripts/location_list.php'); ?>
-    </table>
-    <br>
-    <button id="refreshbtn"><i class="fa fa-refresh"></i> Refresh</button>
+    </table>  
   </center>
 </div>
     </body>
--- IV Pump Tracker/scripts/location_list.php
+++ IV Pump Tracker/scripts/location_list.php
@@ -12,69 +12,103 @@
     echo "<tr><thead><th>&nbsp;</th><th>Brier</th><th>Manor</th><th>Springfield</th><th>Encore</th></thead></tr>";
     echo "<tr><td>#Encore001</td><td draggable='true' id='brier1'>";
     if($locations['brier1']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo"</td><td draggable='true' id='manor1'>";
     if($locations['manor1']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='springfield1'>";
     if($locations['springfield1']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='encore1'>";
     if($locations['encore1']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td></tr>";
         echo "<tr><td>#Encore002</td><td draggable='true' id='brier2'>";
     if($locations['brier2']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo"</td><td draggable='true' id='manor2'>";
     if($locations['manor2']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='springfield2'>";
     if($locations['springfield2']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='encore2'>";
     if($locations['encore2']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td></tr>";
         echo "<tr><td>#Encore003</td><td draggable='true' id='brier3'>";
     if($locations['brier3']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo"</td><td draggable='true' id='manor3'>";
     if($locations['manor3']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='springfield3'>";
     if($locations['springfield3']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='encore3'>";
     if($locations['encore3']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td></tr>";
         echo "<tr><td>#Encore004</td><td draggable='true' id='brier4'>";
     if($locations['brier4']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo"</td><td draggable='true' id='manor4'>";
     if($locations['manor4']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='springfield4'>";
     if($locations['springfield4']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td><td draggable='true' id='encore4'>";
     if($locations['encore4']==1){
-        echo "<i class='fa fa-check fa-3x'></i>";
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo "</td></tr>";
+            echo "<tr><td>#Encore005</td><td draggable='true' id='brier5'>";
+    if($locations['brier5']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo"</td><td draggable='true' id='manor5'>";
+    if($locations['manor5']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo "</td><td draggable='true' id='springfield5'>";
+    if($locations['springfield5']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo "</td><td draggable='true' id='encore5'>";
+    if($locations['encore5']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo "</td></tr>";
+            echo "<tr><td>#Encore006</td><td draggable='true' id='brier6'>";
+    if($locations['brier6']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo"</td><td draggable='true' id='manor6'>";
+    if($locations['manor6']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo "</td><td draggable='true' id='springfield6'>";
+    if($locations['springfield6']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
+    }
+    echo "</td><td draggable='true' id='encore6'>";
+    if($locations['encore6']==1){
+        echo "<i class='fa fa-check fa-2x'></i>";
     }
     echo "</td></tr>";
